da98bf476481c4338e38929a7d7f6d374b3b8eaf
xiongwei.zhu <zhuxiongwei@foxmail.com>
2019-03-30 13:25:38 +0800

    react hook demo 2

--- src/components/ReactHook/HookDemo.js
+++ src/components/ReactHook/HookDemo.js
@@ -1,13 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
 
-// 每次组件更新的时候，函数组件都会重新执行一遍，但是不意思着组件卸载重装
-const Component = props => {
-  console.log('--------> render child');
-  const [num, setNum] = useState(0);
+const Api = function() {
+  const fns = [];
+  let mState = false;
+
+  this.register = (fn) => {
+    fns.push(fn);
+  };
+  this.delete = (fn) => {
+    console.log('delete function', fns.length);
+    for (const mFn of fns) {
+      if (mFn === fn) {
+        const idx = fns.indexOf(fn);
+        fns.splice(idx, 1);
+      }
+    }
+  };
+  this.setState = state => {
+    mState = state;
+    for (const fn of fns) {
+      fn(mState);
+    }
+  };
+};
+
+window.api = new Api();
+
+function useStatus() {
+  const [isOnline, setIsOnline] = useState(false);
 
   useEffect(() => {
-    console.log('useEffect child');
+    console.log('useEffect');
+    const handleState = (state) => {
+      setIsOnline(state);
+    };
+
+    window.api.register(handleState);
+
+    return () => {
+      window.api.delete(handleState);
+    };
+  });
+
+  return isOnline;
+}
+
+// 每次组件更新的时候，函数组件都会重新执行一遍，但是不意味着组件卸载重装
+const Component = () => {
+  const isOnline = useStatus();
+
+  useEffect(() => {
 
     return () => {
       // 组件更新／卸截时，这个都会被调用
@@ -17,8 +60,7 @@ const Component = props => {
 
   return (
     <>
-      <span onClick={() => setNum(num + 1)}>{num}</span>
-      <span>{props.name}</span>
+      <p>{isOnline ? 'online' : 'offline'}</p>
     </>
   );
 };
@@ -29,6 +71,8 @@ const App = () => {
     name: 'xiongwei',
     age: 1,
   });
+
+  const isOnline = useStatus();
 
   useEffect(() => {
     // document.title = `you clicked ${count} times`;
@@ -44,7 +88,7 @@ const App = () => {
       <p>times: {count}</p>
       {/* <p>name: {user.name}    age: {user.age}</p> */}
       <button onClick={() => {setCount(count + 1); setUser({...user, name: user.name + count});}}>Click</button>
-
+      <p>name: {isOnline ? 'xiongwei' : 'nobody'}</p>
       <div>
         <button onClick={() => setUser({ ...user, name: user.name + count })}>change</button>
         <Component name={user.name} />
